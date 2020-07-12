fa92a4bdbab52657b60d1d8d3c949a1a9ee5932a
Luis <luis.olivetti@db1.com.br>
2020-07-12 16:50:27 -0300

    Adicionado SeedingService para popular base vazia

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
+using SalesWebMVC.Data;
 
 namespace SalesWebMVC
 {
@@ -42,14 +43,18 @@ namespace SalesWebMVC
             services.AddDbContext<SalesWebMVCContext>(options =>
                 options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder =>
                     builder.MigrationsAssembly("SalesWebMVC")));
+
+            // registra o serviço
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed(); //popular a base
             }
             else
             {
